c0643d383da9988ba34f58f8cb90e3d10449eee3
Akshola00 <akinniyishola07@gmail.com>
2024-03-29 14:54:28 +0100

    pgrs

--- foodmenuapp/urls.py
+++ foodmenuapp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from foodmenuapp import settings
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("food/", include('food.urls')),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
