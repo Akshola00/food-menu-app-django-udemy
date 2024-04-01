4aeb163d04905d5d29f55d66f7e0c61fafbed652
Akshola00 <akinniyishola07@gmail.com>
2024-04-01 08:49:36 +0100

    modified:   users/views.py

--- users/views.py
+++ users/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
-
         form = RegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
@@ -16,6 +15,8 @@ def register(request):
     else:
         form = RegisterForm()
         return render(request, 'users/register.html', {'form':form})
+    
+    
 @login_required()
 def profilepage(request):
     return render(request, 'users/profile.html')
