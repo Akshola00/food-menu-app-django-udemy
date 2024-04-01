b8a86a7ff43b114d324881a75be1896efa2e9075
Akshola00 <akinniyishola07@gmail.com>
2024-04-01 08:50:39 +0100

    modified:   food/views.py

--- food/views.py
+++ food/views.py
@@ -3,25 +3,40 @@ from django.http import HttpResponse
 from food.models import Item
 from django.template import loader
 from .forms import itemform
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+# from django 
 # Create your views here.
-def index(request):
-    item_list = Item.objects.all()
-    context = {
-        "item_list":item_list,
-    }
-    template = loader.get_template('food/index.html')
-    return HttpResponse(template.render(context, request))
+# def index(request):
+#     item_list = Item.objects.all()
+#     context = {
+#         "item_list":item_list,
+#     }
+#     template = loader.get_template('food/index.html')
+#     return HttpResponse(template.render(context, request))
+
+
+class index(ListView):
+    model = Item;
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
+
 
 def item(request):
     return HttpResponse('<h1>this is an item view</h1>')
 
 
-def detail(request, item_id):
-    item = Item.objects.get(id=item_id)
-    context = {
-        "item":item
-    }
-    return render(request, 'food/details.html', context)
+# def detail(request, item_id):
+#     item = Item.objects.get(id=item_id)
+#     context = {
+#         "item":item
+#     }
+#     return render(request, 'food/details.html', context)
+
+class detail(DetailView):
+    model = Item;
+    template_name = 'food/details.html'
 
 def create_item(request):
     form = itemform(request.POST or None)
