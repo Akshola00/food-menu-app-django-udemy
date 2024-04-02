f821ffd68e6216ec9a6a3103e0b6504f1f1be7bd
Akshola00 <akinniyishola07@gmail.com>
2024-04-02 21:37:54 +0100

    modified:   db.sqlite3 	modified:   food/__pycache__/models.cpython-311.pyc 	modified:   food/__pycache__/urls.cpython-311.pyc 	modified:   food/__pycache__/views.cpython-311.pyc 	new file:   food/formerfuncview.txt 	new file:   food/migrations/0005_item_user_name.py 	new file:   food/migrations/__pycache__/0005_item_user_name.cpython-311.pyc 	modified:   food/models.py 	modified:   food/templates/food/item-form.html 	modified:   food/urls.py 	modified:   food/views.py 	modified:   users/templates/users/login.html 	modified:   users/templates/users/register.html

--- food/urls.py
+++ food/urls.py
@@ -3,11 +3,11 @@ from . import views
 # app_name = 'food' name spacnig for where there are two or more apps with same url name
 urlpatterns=[
     path('', views.index.as_view(), name='index' ),
-    path('item/', views.item, name='item' ),
+    # path('item/', views.item, name='item' ),
     path('<int:pk>', views.detail.as_view(), name='details'),
 
     # add view
-    path('add', views.create_item, name='create_item'),
+    path('add', views.create_item.as_view(), name='create_item'),
     path('update/<int:id>/', views.update_food, name='update_food'),
     path('delete/<int:id>/', views.delete, name='delete'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,3 +1,4 @@
+from django.forms import BaseModelForm
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from food.models import Item
@@ -5,15 +6,8 @@ from django.template import loader
 from .forms import itemform
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-# from django 
-# Create your views here.
-# def index(request):
-#     item_list = Item.objects.all()
-#     context = {
-#         "item_list":item_list,
-#     }
-#     template = loader.get_template('food/index.html')
-#     return HttpResponse(template.render(context, request))
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 
 class index(ListView):
@@ -22,28 +16,21 @@ class index(ListView):
     context_object_name = 'item_list'
 
 
-
-def item(request):
-    return HttpResponse('<h1>this is an item view</h1>')
-
-
-# def detail(request, item_id):
-#     item = Item.objects.get(id=item_id)
-#     context = {
-#         "item":item
-#     }
-#     return render(request, 'food/details.html', context)
-
 class detail(DetailView):
     model = Item;
     template_name = 'food/details.html'
 
-def create_item(request):
-    form = itemform(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect("index")
-    return render(request, 'food/item-form.html', context={'form':form})
+
+
+class create_item(CreateView):
+    model = Item;
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
+
 
 def update_food(request, id):
     item = Item.objects.get(id=id)
